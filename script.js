fb3f44d408807558466e518acd5d0cd062a44ebb
crobinson1985 <crobinson92828@gmail.com>
2025-03-28 23:06:26 -0400

    Stuff

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const option2 = document.getElementById('rating2');
 const option3 = document.getElementById('rating3');
 const option4 = document.getElementById('rating4');
 const option5 = document.getElementById('rating5');
+const mainBox = document.getElementById('main-box');
+const thankYouBox = document.getElementById('thank-you-box');
+const submitBtn =  document.getElementById('submit-button');
+const backdrop = document.getElementById('backdrop');
+const errorOkayBtn = document.getElementById('error-ok-button');
+const errorDiv = document.getElementById('error-div');
 
 // let ratingSelection = '';
 // let option = 0;
@@ -25,7 +31,28 @@ function toggleButton(btn) {
 }
 
 function submitFunc() {
-  
+  if (selected){
+    mainBox.style.display = "none";
+    thankYouBox.style.display = "block";
+  } else {
+    backdrop.style.display = 'block';
+    errorDiv.style.display = 'block';
+  }
+}
+function toggleBackdrop() {
+  if (backdrop.style.display == 'none') {
+    backdrop.style.display = 'block';  
+  } else {
+    backdrop.style.display = 'none';
+    errorDiv.style.display = 'none';
+  } 
+}
+
+function errorOkayHandler() {
+  if (errorDiv.style.display === 'block') {
+    errorDiv.style.display = 'none';
+    toggleBackdrop();
+  }
 }
 
 option1.addEventListener('click', ratingSelect);
@@ -33,4 +60,6 @@ option2.addEventListener('click', ratingSelect);
 option3.addEventListener('click', ratingSelect);
 option4.addEventListener('click', ratingSelect);
 option5.addEventListener('click', ratingSelect);
-submitBtn.addEventListener('click', submitFunc)
+submitBtn.addEventListener('click', submitFunc);
+backdrop.addEventListener('click', toggleBackdrop);
+errorOkayBtn.addEventListener('click', errorOkayHandler)
